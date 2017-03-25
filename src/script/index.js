c780c894ae0dd5abb672f37aa286481888c7031b
Todd Griffin <nemagnusfrater@gmail.com>
2017-03-25 02:11:24 -0400

    Possibly finished login.

--- src/script/index.js
+++ src/script/index.js
@@ -26,10 +26,10 @@ window.onclick = function(event) {
 
 // signing up
 function signup () {
-	  var email = document.getElementById('email').value;
+	  var email = document.getElementById('su-email').value;
 
-      var password = document.getElementById('password').value;
-      var repassword = document.getElementById('repassword').value;
+      var password = document.getElementById('su-password').value;
+      var repassword = document.getElementById('su-repassword').value;
 
       if (password.valueOf() != repassword.valueOf()){
       	alert('Passwords don\'t match');
@@ -61,4 +61,40 @@ function signup () {
         console.log(error);
         // [END_EXCLUDE]
       });
+}
+
+function login () {
+
+        var email = document.getElementById('lg-email').value;
+        var password = document.getElementById('lg-password').value;
+
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('login').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+      
+      document.getElementById('login').disabled = true;
 }
