0a1a8be52db780b9528edd3de77f6956896b4fcc
Todd Griffin <nemagnusfrater@gmail.com>
2017-03-26 01:55:28 -0400

    Cleaned up some code

--- src/script/home.js
+++ src/script/home.js
@@ -5,36 +5,6 @@ window.onload = function () {
 	initApp();
 }
 
-function getData(user){
-	console.log('get data req');
-	return new Promise(function(resolve, reject){
-		// check if user data exists yet
-    		var ref = firebase.database().ref().child("user");
-			ref.on('value', function(snapshot) {
-				var count = 0;
-				// cycle through each 'user'
-	    		snapshot.forEach(function(childSnapshot) {
-
-	    			console.log("child: " + childSnapshot);
-
-		    		// get each 'user's data
-		      		var childData = childSnapshot.val();
-		      		count++;
-		      		// if this is the correct user, update found variable
-		      		if (user.email.valueOf() === childData.email.valueOf()) {
-		      			console.log("Found match for user email in database, returning true to 'user data exists'.");
-		      			resolve(true);
-		      		} else if(count === snapshot.length){
-		      			console.log("Resolve false.");
-		      			resolve(false);
-		      		}
-	    		});
-			});
-	});
-}
-
-
-
 function initApp () {
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
